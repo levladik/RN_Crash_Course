f53c665f1d40b3161b7dd82864a7604bafedb17b
Levladik <vladislavtyshenko@yandex.ru>
2024-07-29 18:36:20 +0400

    feat: Add authentication layout and sign-in screen
    - Created `AuthLayout` component with stack navigator for sign-in and sign-up screens
    - Implemented `SignIn` screen with form fields for email and password, logo, and "Sign In" button
    - Added `FormField` component for reusable form input with show/hide password functionality
    - Included navigation to sign-up screen from sign-in screen
    - Configured `StatusBar` with custom background color and light style

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,28 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View, Text } from "react-native";
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+
+      <StatusBar backgroundColor="#161622" style="light"/>
+    </>
   );
 };
 
