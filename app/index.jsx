477b20e00051ec8f918cbb27da6a2609ca259b24
Levladik <vladislavtyshenko@yandex.ru>
2024-07-29 16:13:46 +0400

    feat: Integrate CustomButton into main index
    - Configured button to navigate to the sign-in page on press

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, Text, View, Image } from "react-native";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButtom from "../components/CustomButtom";
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -38,7 +39,7 @@ export default function App() {
 
           <CustomButtom 
             title="Continue with Email"
-            handlePress={() => {}}
+            handlePress={() => router.push('/sign-in')}
             containerStyles='w-full mt-7'
             textStyles=''
           />
