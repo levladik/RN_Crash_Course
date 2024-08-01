145c13d1ab8c7c71f130479888af1b1dc17a827f
Levladik <vladislavtyshenko@yandex.ru>
2024-08-01 13:37:23 +0400

    add SignUp screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -27,11 +27,11 @@ const RootLayout = () => {
 
   return (
     <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="/search/[query]" options={{ headerShown: false }} />
-      </Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="search" options={{ headerShown: false }} />
+    </Stack>
   );
 };
 
