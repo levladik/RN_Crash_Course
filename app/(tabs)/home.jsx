5b5ac6f57c39a490e56ee5e7830af011d9b72ac4
Levladik <vladislavtyshenko@yandex.ru>
2024-08-18 17:03:13 +0400

    feat: Implement search and trending functionality, add latest posts retrieval - Added `latestPost` function to fetch the latest post from Appwrite database - Created `searchPosts` function to perform search queries on posts - Implemented search functionality in `SearchInput` component for real-time filtering - Rendered trending posts in the Trending section

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
-  Alert,
   FlatList,
   Image,
   RefreshControl,
@@ -13,12 +12,13 @@ import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import { getAllPosts } from "../../lib/appwrite";
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
   const [refreshing, setrefreshing] = useState(false);
 
   const onRefresh = async () => {
@@ -59,11 +59,7 @@ const Home = () => {
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Latest Videos
               </Text>
-              <Trending
-                posts={
-                  [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }] ?? []
-                }
-              />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
